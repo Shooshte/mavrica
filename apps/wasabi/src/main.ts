444a8b0a1e120e49f5320da92e8bf38fed63a8f3
shooshte <miha.sustersic.work@gmail.com>
2022-06-12 22:09:33 +0200

    wasabi setup

--- apps/wasabi/src/main.ts
+++ apps/wasabi/src/main.ts
@@ -1,18 +1,29 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
+import 'dotenv';
+import tus = require('tus-node-server');
 
-import * as express from 'express';
-
-const app = express();
-
-app.get('/api', (req, res) => {
-  res.send({ message: 'Welcome to wasabi!' });
+const server = new tus.Server();
+server.datastore = new tus.FileStore({
+  path: '/files',
 });
 
-const port = process.env.port || 3333;
-const server = app.listen(port, () => {
-  console.log(`Listening at http://localhost:${port}/api`);
+const host = '127.0.0.1';
+const port = 1080;
+
+// @ts-expect-error: ts-node-server imports listed arguments types are wrong according to their docs
+server.listen({ host, port }, () => {
+  console.log(
+    `[${new Date().toLocaleTimeString()}] tus server listening at http://${host}:${port}`
+  );
+
+  console.log('process.env: ', process.env);
+
+  server.datastore = new tus.S3Store({
+    path: '/files',
+    bucket: 'mavrica',
+    accessKeyId: process.env.NX_WASABI_KEY,
+    secretAccessKey: process.env.NX_WASABI_SECRET,
+    region: 'eu-west-1',
+    partSize: 8 * 1024, // each uploaded part will have ~8MB,
+    tmpDirPrefix: 'tus-s3-store',
+  });
 });
-server.on('error', console.error);
