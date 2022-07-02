5559e4ab2d5546338d6056260d6f38acc3114115
shooshte <miha.sustersic.work@gmail.com>
2022-07-02 09:48:52 +0200

    added image-parser-port to .env

--- apps/image-parser/src/main.ts
+++ apps/image-parser/src/main.ts
@@ -14,7 +14,7 @@ app.get('/api', (req, res) => {
 app.get('/api/wasabi', (req, res, next) => wasabi(req, res, next));
 app.use((err, req, res, next) => handleError(err, req, res));
 
-const port = process.env.port || 3333;
+const port = process.env.NX_IMAGE_PARSER_PORT || 3333;
 const server = app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}/api`);
 });
