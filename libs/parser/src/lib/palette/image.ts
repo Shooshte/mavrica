3c7da685ac1d7cd2d1774c717e2f46489a4dd57a
shooshte <miha.sustersic.work@gmail.com>
2022-07-10 09:08:57 +0200

    removed image-parser BE, commented out the image parsing lib in order to build FE

--- libs/parser/src/lib/palette/image.ts
+++ libs/parser/src/lib/palette/image.ts
@@ -1,147 +1,151 @@
-import { createCanvas, loadImage } from 'canvas';
-import { getRelativeLuminance, RGBToHex } from '../color';
+// import { Blob } from 'buffer';
+// import { getRelativeLuminance, RGBToHex } from '../color';
 
-interface ResizeReturn {
-  imageBuffer: Buffer;
-  minInclusionCount: number;
-}
+// interface ResizeReturn {
+//   imageBuffer: Buffer;
+//   minInclusionCount: number;
+// }
 
-const INCLUSION_PERCENTAGE = 0.01;
-const MAX_IMAGE_SIZE = 500; // max width or height of the image
+// const INCLUSION_PERCENTAGE = 0.01;
+// const MAX_IMAGE_SIZE = 500; // max width or height of the image
 
-export const resizeImage = async (
-  imageBuffer: Buffer
-): Promise<ResizeReturn> => {
-  const image = await loadImage(imageBuffer);
+// export const resizeImage = async (
+//   imageBuffer: Buffer
+// ): Promise<ResizeReturn> => {
 
-  const height = image.height;
-  const width = image.width;
+//   const DOMCanvas = document.createElement("CANVAS");
+//   const blob = new Blob([imageBuffer])
 
-  // If image is already small enough, no need to resize
-  if (height <= MAX_IMAGE_SIZE && width <= MAX_IMAGE_SIZE) {
-    const allPixels = height * width;
-    const minInclusionCount = (INCLUSION_PERCENTAGE * allPixels) / 100;
+//   const image = await loadImage(imageBuffer);
 
-    return { imageBuffer: imageBuffer, minInclusionCount };
-  }
+//   const height = image.height;
+//   const width = image.width;
 
-  const canvas = createCanvas(width, height);
-  const ctx = canvas.getContext('2d');
+//   // If image is already small enough, no need to resize
+//   if (height <= MAX_IMAGE_SIZE && width <= MAX_IMAGE_SIZE) {
+//     const allPixels = height * width;
+//     const minInclusionCount = (INCLUSION_PERCENTAGE * allPixels) / 100;
 
-  let desiredHeight = height;
-  let desiredWidth = width;
+//     return { imageBuffer: imageBuffer, minInclusionCount };
+//   }
 
-  if (height > MAX_IMAGE_SIZE || width > MAX_IMAGE_SIZE) {
-    const scaleToHeight = height > width;
-    if (scaleToHeight) {
-      desiredHeight = MAX_IMAGE_SIZE;
-      const scaleBy = MAX_IMAGE_SIZE / height;
-      desiredWidth = width * scaleBy;
-    } else {
-      desiredWidth = MAX_IMAGE_SIZE;
-      const scaleBy = MAX_IMAGE_SIZE / width;
-      desiredHeight = height * scaleBy;
-    }
-  }
+//   const canvas = createCanvas(width, height);
+//   const ctx = canvas.getContext('2d');
 
-  canvas.width = desiredWidth;
-  canvas.height = desiredHeight;
+//   let desiredHeight = height;
+//   let desiredWidth = width;
 
-  ctx.drawImage(image, 0, 0, canvas.width, canvas.height);
-  const newBuffer = canvas.toBuffer();
+//   if (height > MAX_IMAGE_SIZE || width > MAX_IMAGE_SIZE) {
+//     const scaleToHeight = height > width;
+//     if (scaleToHeight) {
+//       desiredHeight = MAX_IMAGE_SIZE;
+//       const scaleBy = MAX_IMAGE_SIZE / height;
+//       desiredWidth = width * scaleBy;
+//     } else {
+//       desiredWidth = MAX_IMAGE_SIZE;
+//       const scaleBy = MAX_IMAGE_SIZE / width;
+//       desiredHeight = height * scaleBy;
+//     }
+//   }
 
-  const allPixels = desiredHeight * desiredWidth;
-  const minInclusionCount = (INCLUSION_PERCENTAGE * allPixels) / 100;
+//   canvas.width = desiredWidth;
+//   canvas.height = desiredHeight;
 
-  return { imageBuffer: newBuffer, minInclusionCount };
-};
+//   ctx.drawImage(image, 0, 0, canvas.width, canvas.height);
+//   const newBuffer = canvas.toBuffer();
 
-export const getPixels = async (imageBuffer: Buffer) => {
-  const data = {};
-  const image = await loadImage(imageBuffer);
-  const height = image.height;
-  const width = image.width;
-  const canvas = createCanvas(width, height);
-  const ctx = canvas.getContext('2d');
-  ctx.drawImage(image, 0, 0, width, height);
+//   const allPixels = desiredHeight * desiredWidth;
+//   const minInclusionCount = (INCLUSION_PERCENTAGE * allPixels) / 100;
 
-  const imageData = ctx.getImageData(0, 0, width, height);
+//   return { imageBuffer: newBuffer, minInclusionCount };
+// };
 
-  for (let i = 0; i < imageData.data.length; i += 4) {
-    const red = imageData.data[i];
-    const green = imageData.data[i + 1];
-    const blue = imageData.data[i + 2];
+// export const getPixels = async (imageBuffer: Buffer) => {
+//   const data = {};
+//   const image = await loadImage(imageBuffer);
+//   const height = image.height;
+//   const width = image.width;
+//   const canvas = createCanvas(width, height);
+//   const ctx = canvas.getContext('2d');
+//   ctx.drawImage(image, 0, 0, width, height);
 
-    const relativeLuminance = getRelativeLuminance({
-      r: red,
-      g: green,
-      b: blue,
-    });
-    const roundedLuminance = roundToNearest(relativeLuminance, 11, 100);
-    const hex = RGBToHex({ r: red, g: green, b: blue });
+//   const imageData = ctx.getImageData(0, 0, width, height);
 
-    // @ts-ignore this is throwaway code, will refactor after I am satisfied with parsing results
-    const luminanceIncluded = data[roundedLuminance] !== undefined;
+//   for (let i = 0; i < imageData.data.length; i += 4) {
+//     const red = imageData.data[i];
+//     const green = imageData.data[i + 1];
+//     const blue = imageData.data[i + 2];
 
-    if (!luminanceIncluded) {
-      // @ts-ignore this is throwaway code, will refactor after I am satisfied with parsing results
-      data[roundedLuminance] = {
-        count: 1,
-        colors: [
-          {
-            hex,
-            count: 1,
-            relativeLuminance,
-          },
-        ],
-        roundedLuminance,
-      };
-    } else {
-      // @ts-ignore this is throwaway code, will refactor after I am satisfied with parsing results
-      const newColors = data[roundedLuminance].colors.some(
-        // @ts-ignore this is throwaway code, will refactor after I am satisfied with parsing results
-        (color) => color.hex === hex
-      )
-        ? // @ts-ignore this is throwaway code, will refactor after I am satisfied with parsing results
-          data[roundedLuminance].colors.map((color) => {
-            if (color.hex === hex) {
-              return {
-                ...color,
-                count: color.count + 1,
-              };
-            }
-            return {
-              ...color,
-            };
-          })
-        : [
-            // @ts-ignore this is throwaway code, will refactor after I am satisfied with parsing results
-            ...data[roundedLuminance].colors,
-            { hex, count: 1, relativeLuminance },
-          ];
-      // @ts-ignore this is throwaway code, will refactor after I am satisfied with parsing results
-      data[roundedLuminance] = {
-        // @ts-ignore this is throwaway code, will refactor after I am satisfied with parsing results
-        count: data[roundedLuminance].count + 1,
-        // @ts-ignore this is throwaway code, will refactor after I am satisfied with parsing results
-        colors: newColors.sort((a, b) => {
-          if (a.count === b.count) {
-            return b.relativeLuminance - a.relativeLuminance;
-          }
-          return b.count - a.count;
-        }),
-        roundedLuminance,
-      };
-    }
-  }
+//     const relativeLuminance = getRelativeLuminance({
+//       r: red,
+//       g: green,
+//       b: blue,
+//     });
+//     const roundedLuminance = roundToNearest(relativeLuminance, 11, 100);
+//     const hex = RGBToHex({ r: red, g: green, b: blue });
 
-  return data;
-};
+//     // @ts-ignore this is throwaway code, will refactor after I am satisfied with parsing results
+//     const luminanceIncluded = data[roundedLuminance] !== undefined;
 
-const roundToNearest = (arg: number, roundTo: number, max: number) => {
-  const roundedNumber = Math.round(arg / roundTo) * roundTo;
-  if (roundedNumber > max) {
-    return max;
-  }
-  return roundedNumber;
-};
+//     if (!luminanceIncluded) {
+//       // @ts-ignore this is throwaway code, will refactor after I am satisfied with parsing results
+//       data[roundedLuminance] = {
+//         count: 1,
+//         colors: [
+//           {
+//             hex,
+//             count: 1,
+//             relativeLuminance,
+//           },
+//         ],
+//         roundedLuminance,
+//       };
+//     } else {
+//       // @ts-ignore this is throwaway code, will refactor after I am satisfied with parsing results
+//       const newColors = data[roundedLuminance].colors.some(
+//         // @ts-ignore this is throwaway code, will refactor after I am satisfied with parsing results
+//         (color) => color.hex === hex
+//       )
+//         ? // @ts-ignore this is throwaway code, will refactor after I am satisfied with parsing results
+//           data[roundedLuminance].colors.map((color) => {
+//             if (color.hex === hex) {
+//               return {
+//                 ...color,
+//                 count: color.count + 1,
+//               };
+//             }
+//             return {
+//               ...color,
+//             };
+//           })
+//         : [
+//             // @ts-ignore this is throwaway code, will refactor after I am satisfied with parsing results
+//             ...data[roundedLuminance].colors,
+//             { hex, count: 1, relativeLuminance },
+//           ];
+//       // @ts-ignore this is throwaway code, will refactor after I am satisfied with parsing results
+//       data[roundedLuminance] = {
+//         // @ts-ignore this is throwaway code, will refactor after I am satisfied with parsing results
+//         count: data[roundedLuminance].count + 1,
+//         // @ts-ignore this is throwaway code, will refactor after I am satisfied with parsing results
+//         colors: newColors.sort((a, b) => {
+//           if (a.count === b.count) {
+//             return b.relativeLuminance - a.relativeLuminance;
+//           }
+//           return b.count - a.count;
+//         }),
+//         roundedLuminance,
+//       };
+//     }
+//   }
+
+//   return data;
+// };
+
+// const roundToNearest = (arg: number, roundTo: number, max: number) => {
+//   const roundedNumber = Math.round(arg / roundTo) * roundTo;
+//   if (roundedNumber > max) {
+//     return max;
+//   }
+//   return roundedNumber;
+// };
